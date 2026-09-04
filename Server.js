298b84af3adc1f8a005dfc4a5405c8e3c3a12c4b
tirupatamma <tirupatamma@gmail.com>
2026-09-04 11:07:36 +0530

    dagtabase schema

--- Server.js
+++ Server.js
@@ -2,7 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userData = require('./model');
 const app = express();
+app.use(express.json());
 
-mongoose.connect("").then(()=>console.log("database connected")).catch((err)=> console.log(err.message))
+mongoose.connect("mongodb+srv://tirupatammajavisetty_db_user:71a3k6cYmuTfUHtp@cluster0.lkz2wgb.mongodb.net/").then(()=>console.log("database connected")).catch((err)=> console.log(err.message))
+app.post('/send',async (req,res)=>{
+    const{username,email,password}= req.body;
+    try{
+        const data=new userData({
+            username,
+            email,
+            password
+        });
+        await data.save();
+        return res.json({"message": "Data sent successfully"});
+    }
+    catch (err){
+        console.log(err.message)
+    }
+}) 
+
 
 app.listen(3000, ()=> console.log("server is running....."))
+
